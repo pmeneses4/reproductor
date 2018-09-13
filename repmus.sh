2e17f91b9df775dbd2e4d1443a5d242a0c6dafa5
pmeneses4 <pollo_desu@comunidad.unam.mx>
2018-09-12 19:41:39 -0500

    Primer commit

---
+++ repmus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Programa reproductor de musica
+
+clear
+
+echo "..:: Reproductor musical ::.."
+
+#Invoca al comando de mpg123 para iniciar el reproductor musical
+command mpg123;
+
+
+#el comando trap nos permite atrapar señales, en este caso, buscara aquellas con formato mp3 para el reproductor musical 
+trap "*.mp3";
+
+#$@ : grupo de parametros del script
+#echo IDa: $(!) y $@   #Este renglon es editable, ya que fue encontrado en el manual de 4 party
+
+
